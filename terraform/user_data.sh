5ffa18f3ba3b49d25039e5d66fc17073273252d8
Samjin97 <162026674+Samjin97@users.noreply.github.com>
2025-08-30 06:16:23 +0100

    Update user_data.sh

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -7,8 +7,7 @@ sudo systemctl start docker
 sudo systemctl enable docker
 
 # Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" \
-     -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Configure CloudWatch Agent
