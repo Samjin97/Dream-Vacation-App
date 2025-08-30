677889454533fd346585b562072b250f7d381d6b
Samjin97 <162026674+Samjin97@users.noreply.github.com>
2025-08-30 12:25:13 +0100

    updated docker compose and ci/cd workflow

--- terraform/user_data.sh
+++ terraform/user_data.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 sudo apt-get update -y
-sudo apt-get install -y docker.io amazon-cloudwatch-agent
+sudo apt-get install -y docker.io
 
-# Enable Docker
-sudo systemctl start docker
+# Enable & start Docker
 sudo systemctl enable docker
+sudo systemctl start docker
 
 # Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" \
      -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Configure CloudWatch Agent
+# Add ubuntu user to docker group
+sudo usermod -aG docker ubuntu
+newgrp docker <<EONG
+echo "Docker group applied to ubuntu user"
+EONG
+
+# Install CloudWatch Agent
+sudo apt-get install -y amazon-cloudwatch-agent
 cat <<EOF | sudo tee /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 {
   "metrics": { "metrics_collected": { "cpu": { "measurement": ["cpu_usage_idle","cpu_usage_user","cpu_usage_system"], "metrics_collection_interval": 60 } } }
 }
 EOF
-
-# Start CloudWatch Agent
 sudo systemctl enable amazon-cloudwatch-agent
 sudo systemctl start amazon-cloudwatch-agent
-
